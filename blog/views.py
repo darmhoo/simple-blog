b7b114742df9c75597334f86c62675a37795f862
Omodamola <darmhoo@hotmail.com>
2018-08-17 11:51:22 +0100

    make it better

--- blog/views.py
+++ blog/views.py
@@ -10,11 +10,13 @@ from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.contrib.postgres.search import TrigramSimilarity
 # Create your views here.
 
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -35,6 +37,7 @@ def post_list(request, tag_slug=None):
                   {'posts': posts,
                    'page': page,
                    'tag': tag})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,slug=post,
